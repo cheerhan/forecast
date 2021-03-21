ae66e20696ab8581538d1a4e0f0995dbb9b5a5ce
Cheer Han <lala.gu@live.cn>
2021-03-21 11:41:30 +0800

    增加读实时监控电站列表API处理。

---
+++ readHistoryAPI.R
@@ -0,0 +1,34 @@
+library(rjson)
+library(dplyr)
+library(tidyr)
+library(httr)
+library(lubridate)
+
+##历史测点数据header
+hdr=c('Accept'="application/json, text/plain, */*",
+      'Accept-Encoding'="gzip, deflate, br",
+      'Accept-Language'="en,zh-CN;q=0.9,zh;q=0.8,la;q=0.7,ru;q=0.6" ,
+      'Authorization'="bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJwbHNhZG1pbiIsImVudGVycHJpc2VDb2RlIjoxMDIyLCJ1c2VyRW50ZXJwcmlzZVN0YXR1cyI6MywiYXV0byI6IjAiLCJ1c2VyU3RhdHVzIjoxLCJ1c2VyX25hbWUiOiJwbHNhZG1pbiIsImNvbXBhbnlOYW1lIjpudWxsLCJ1c2VyRnVsbE5hbWUiOiLmma7mtJvmlq_nrqHnkIblkZgiLCJwaG9uZU51bSI6IjE1NjExNDA5NTYyIiwidXNlckxvZ28iOm51bGwsInVzZXJJZCI6IjQ1MTQzNjQ2MzY5MjI4OCIsImF1dGhvcml0aWVzIjpbImFkbWluIl0sImNsaWVudF9pZCI6ImNuZWdyb3VwIiwic2NvcGUiOlsiYWxsIiwicmVhZCIsIndyaXRlIl0sImVudGVycHJpc2VJZCI6IjQ1MTQzNjQ2Nzg4NjU5MiIsImV4cCI6MTkzMTY1NDIzNSwiZW50ZXJwcmlzZUxvZ28iOiJodHRwczovL3Bvd2VyLmNuZWNsb3VkLmNvbS9hcGkvdjMvaW1hZ2VzLzBjMTM3OTczMDI1MDQ2ODY4OGM5M2ZmZmEyZTg3ODU1LmpwZyIsImVudGVycHJpc2VOYW1lIjoi5pmu5p6r5paw6IO95rqQIiwianRpIjoiY2IyMGVlM2YtMmI3NS00MjE0LWE4ZjYtNjdkODIzYWE0M2VkIiwidXNlcm5hbWUiOiJwbHNhZG1pbiJ9.qLK9ocTltrvrd8ameGhd8Sc_hLvAvhbjy8vKmmKdtVA",      'Content-Type'="application/json;charset=UTF-8")
+
+
+##读devicechart的数据
+request_body <- list(
+  stationCode='3344112',
+  deviceFullCodes=c("3344112M206M1M1","3344112M206M1M2"),
+  startTime="2021-03-18T16:00:00Z",
+  endTime="2021-03-19T04:40:46Z",
+  devicePoints=c("266144","489624","489629","489632"),
+  timeInterval="10",
+  devicePoint="[]",
+  enterpriseId="451436467886592"
+)
+
+##Converting the Request body(Dataframe) to Request body(JSON)
+request_body_json <- toJSON(request_body)
+cat(request_body_json)
+result<-POST("https://power.cnecloud.com/api/v3/wind/analysis/history/devicechart",
+             body=request_body_json,
+             content_type_json(),
+             add_headers(.headers = hdr))
+output<-content(result)
+
---
+++ readStation.R
@@ -0,0 +1,29 @@
+library(rjson)
+library(dplyr)
+library(tidyr)
+library(httr)
+library(lubridate)
+
+station<- function(hdr){
+  time<-Sys.time() %>%
+    as.POSIXct("Asia/Shanghai")%>%
+    format(tz="UTC")%>%
+    gsub(pattern=" ",replacement = "T",time)%>%
+    paste("Z",sep = "")
+  station_requst<- list(
+    UTCString=time,
+    regionName="全部区域"
+  )
+  ##Converting the Request body(Dataframe) to Request body(JSON)
+  station_requst <- toJSON(station_requst)
+  result<-POST("https://power.cnecloud.com/api/v3/pv/monitor/stations",
+               body=station_requst,
+               content_type_json(),
+               add_headers(.headers = hdr))
+  output<-content(result)
+  
+  #读电站列表
+  stations <- output$data$stationDataList
+  stations <- as.data.frame(do.call(rbind, lapply(stations, as.vector)))
+  return(stations)
+}
